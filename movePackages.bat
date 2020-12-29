f29b4b466fb4f7757bfdbd13427be0e92c9679af
drumath <2237k.drumath@gmail.com>
2020-12-30 02:29:33 +0900

    :package: k4a package install

--- movePackages.bat
+++ movePackages.bat
@@ -1,1 +1,6 @@
-COPY .\External\Package\Microsoft.Azure.Kinect.Sensor.1.4.1\lib\native\amd64\release\depthengine_2_0.dll .\Assets\Packages\depthengine_2_0.dll
+COPY .\External\Package\Microsoft.Azure.Kinect.Sensor.1.4.1\lib\native\amd64\release\depthengine_2_0.dll .\Assets\K4A-VFX\Plugins\depthengine_2_0.dll
+COPY .\External\Package\Microsoft.Azure.Kinect.Sensor.1.4.1\lib\native\amd64\release\k4a.dll .\Assets\K4A-VFX\Plugins\k4a.dll
+COPY .\External\Package\System.Memory.4.5.3\lib\netstandard2.0\System.Memory.dll .\Assets\K4A-VFX\Plugins\System.Memory.dll
+COPY .\External\Package\System.Numerics.Vectors.4.5.0\lib\netstandard2.0\System.Numerics.Vectors.dll .\Assets\K4A-VFX\Plugins\System.Numerics.Vectors.dll
+COPY .\External\Package\System.Runtime.CompilerServices.Unsafe.4.6.0\lib\netstandard2.0\System.Runtime.CompilerServices.Unsafe.dll .\Assets\K4A-VFX\Plugins\System.Runtime.CompilerServices.Unsafe.dll
+COPY .\External\Package\System.Buffers.4.5.1\lib\netstandard2.0\System.Buffers.dll .\Assets\K4A-VFX\Plugins\System.Buffers.dll
