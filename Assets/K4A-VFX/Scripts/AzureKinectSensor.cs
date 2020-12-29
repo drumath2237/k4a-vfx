98bc4fb41cea8f5a943f3a38e684edd30850de84
drumath <2237k.drumath@gmail.com>
2020-12-30 02:53:14 +0900

    :tada: Kinectが起動した！

---
+++ Assets/K4A-VFX/Scripts/AzureKinectSensor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Microsoft.Azure.Kinect.Sensor;
+using UnityEngine.UI;
+
+namespace K4A.VFX
+{
+    public class AzureKinectSensor : MonoBehaviour
+    {
+        private Device kinect;
+
+        private void Start()
+        {
+            kinect = Device.Open();
+            kinect.StartCameras(new DeviceConfiguration
+            {
+                ColorFormat = ImageFormat.ColorBGRA32,
+                ColorResolution = ColorResolution.R1080p,
+                DepthMode = DepthMode.NFOV_2x2Binned,
+                SynchronizedImagesOnly = true,
+                CameraFPS = FPS.FPS30
+            });
+            
+        }
+
+        private void OnApplicationQuit()
+        {
+            if (kinect != null)
+            {
+                kinect.StopCameras();
+                kinect.Dispose();
+            }
+        }
+    }
+}
--- movePackages.bat
+++ movePackages.bat
@@ -4,3 +4,4 @@ COPY .\External\Package\System.Memory.4.5.3\lib\netstandard2.0\System.Memory.dll
 COPY .\External\Package\System.Numerics.Vectors.4.5.0\lib\netstandard2.0\System.Numerics.Vectors.dll .\Assets\K4A-VFX\Plugins\System.Numerics.Vectors.dll
 COPY .\External\Package\System.Runtime.CompilerServices.Unsafe.4.6.0\lib\netstandard2.0\System.Runtime.CompilerServices.Unsafe.dll .\Assets\K4A-VFX\Plugins\System.Runtime.CompilerServices.Unsafe.dll
 COPY .\External\Package\System.Buffers.4.5.1\lib\netstandard2.0\System.Buffers.dll .\Assets\K4A-VFX\Plugins\System.Buffers.dll
+COPY .\External\Package\Microsoft.Azure.Kinect.Sensor.1.4.1\lib\netstandard2.0\Microsoft.Azure.Kinect.Sensor.dll .\Assets\K4A-VFX\Plugins
